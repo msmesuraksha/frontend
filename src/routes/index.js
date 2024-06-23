ea7de839895142eb519b7b052924e014cfe06205
msmesuraksha <msmesuraksha9@gmail.com>
2024-06-24 00:21:30 +0530

    remove some

--- src/routes/index.js
+++ src/routes/index.js
@@ -105,22 +105,17 @@ import SparklineChart from "../pages/Charts/SparklineChart";
 import ChartsKnob from "../pages/Charts/charts-knob";
 import ReCharts from "../pages/Charts/ReCharts";
 
-// Maps
-import MapsGoogle from "../pages/Maps/MapsGoogle";
-import MapsVector from "../pages/Maps/MapsVector";
-import MapsLeaflet from "../pages/Maps/MapsLeaflet";
 
-//Icons
-import IconBoxicons from "../pages/Icons/IconBoxicons";
-import IconDripicons from "../pages/Icons/IconDripicons";
-import IconMaterialdesign from "../pages/Icons/IconMaterialdesign";
-import IconFontawesome from "../pages/Icons/IconFontawesome";
+
+
+
+
+
+
 
 //Tables
-import BasicTables from "../pages/Tables/BasicTables";
-import DatatableTables from "../pages/Tables/DatatableTables";
-import ResponsiveTables from "../pages/Tables/ResponsiveTables";
-import DragDropTables from "../pages/Tables/DragDropTables";
+
+
 
 //Blog
 import BlogList from "../pages/Blog/BlogList/index";
@@ -136,49 +131,15 @@ import ApplyJobs from "../pages/JobPages/ApplyJobs/index";
 import CandidateList from "../pages/JobPages/CandidateList";
 import CandidateOverview from "../pages/JobPages/CandidateOverview";
 
-// Forms
-import FormElements from "../pages/Forms/FormElements";
-import FormLayouts from "../pages/Forms/FormLayouts";
-import FormAdvanced from "../pages/Forms/FormAdvanced";
-import FormEditors from "../pages/Forms/FormEditors";
-import FormValidations from "../pages/Forms/FormValidations";
-import FormMask from "../pages/Forms/FormMask";
-import FormRepeater from "../pages/Forms/FormRepeater";
-import FormUpload from "../pages/Forms/FormUpload";
-import FormWizard from "../pages/Forms/FormWizard";
 
-//Ui
-import UiAlert from "../pages/Ui/UiAlert";
-import UiButtons from "../pages/Ui/UiButtons";
-import UiCards from "../pages/Ui/UiCards";
-import UiCarousel from "../pages/Ui/UiCarousel";
-import UiColors from "../pages/Ui/UiColors";
-import UiDropdown from "../pages/Ui/UiDropdown";
-import UiGeneral from "../pages/Ui/UiGeneral";
-import UiGrid from "../pages/Ui/UiGrid";
-import UiImages from "../pages/Ui/UiImages";
-import UiLightbox from "../pages/Ui/UiLightbox";
-import UiModal from "../pages/Ui/UiModal";
-import UiProgressbar from "../pages/Ui/UiProgressbar";
-import UiTabsAccordions from "../pages/Ui/UiTabsAccordions";
-import UiTypography from "../pages/Ui/UiTypography";
-import UiVideo from "../pages/Ui/UiVideo";
-import UiSessionTimeout from "../pages/Ui/UiSessionTimeout";
-import UiRating from "../pages/Ui/UiRating";
-import UiRangeSlider from "../pages/Ui/UiRangeSlider";
-import UiNotifications from "../pages/Ui/ui-notifications";
-import UiOffCanvas from "pages/Ui/UiOffCanvas";
-import UiUtilitie from "../pages/Ui/UiUtilitie";
-import UiPlaceholders from "../pages/Ui/UiPlaceholders";
-import UiToasts from "../pages/Ui/UiToast";
+
+
+
 
 //Pages
-import PagesStarter from "../pages/Utility/pages-starter";
+
 import PagesMaintenance from "../pages/Utility/pages-maintenance";
 import PagesComingsoon from "../pages/Utility/pages-comingsoon";
-import PagesTimeline from "../pages/Utility/pages-timeline";
-import PagesFaqs from "../pages/Utility/pages-faqs";
-import PagesPricing from "../pages/Utility/pages-pricing";
 import Pages404 from "../pages/Utility/pages-404";
 import Pages500 from "../pages/Utility/pages-500";
 import MemberCard from "../pages/admin/member-card/memberCard";
@@ -320,67 +281,17 @@ const authProtectedRoutes = [
   { path: "/charts-knob", component: <ChartsKnob /> },
   { path: "/re-charts", component: <ReCharts /> },
 
-  // Icons
-  { path: "/icons-boxicons", component: <IconBoxicons /> },
-  { path: "/icons-dripicons", component: <IconDripicons /> },
-  { path: "/icons-materialdesign", component: <IconMaterialdesign /> },
-  { path: "/icons-fontawesome", component: <IconFontawesome /> },
 
-  // Tables
-  { path: "/tables-basic", component: <BasicTables /> },
-  { path: "/tables-datatable", component: <DatatableTables /> },
-  { path: "/tables-responsive", component: <ResponsiveTables /> },
-  { path: "/tables-dragndrop", component: <DragDropTables /> },
+
+
 
   // Maps
-  { path: "/maps-google", component: <MapsGoogle /> },
-  { path: "/maps-vector", component: <MapsVector /> },
-  { path: "/maps-leaflet", component: <MapsLeaflet /> },
+ 
 
-  // Forms
-  { path: "/form-elements", component: <FormElements /> },
-  { path: "/form-layouts", component: <FormLayouts /> },
-  { path: "/form-advanced", component: <FormAdvanced /> },
-  { path: "/form-editors", component: <FormEditors /> },
-  { path: "/form-mask", component: <FormMask /> },
-  { path: "/form-repeater", component: <FormRepeater /> },
-  { path: "/form-uploads", component: <FormUpload /> },
-  { path: "/form-wizard", component: <FormWizard /> },
-  { path: "/form-validation", component: <FormValidations /> },
 
-  // Ui
-  { path: "/ui-alerts", component: <UiAlert /> },
-  { path: "/ui-buttons", component: <UiButtons /> },
-  { path: "/ui-cards", component: <UiCards /> },
-  { path: "/ui-carousel", component: <UiCarousel /> },
-  { path: "/ui-colors", component: <UiColors /> },
-  { path: "/ui-dropdowns", component: <UiDropdown /> },
-  { path: "/ui-general", component: <UiGeneral /> },
-  { path: "/ui-grid", component: <UiGrid /> },
-  { path: "/ui-images", component: <UiImages /> },
-  { path: "/ui-lightbox", component: <UiLightbox /> },
-  { path: "/ui-modals", component: <UiModal /> },
-  { path: "/ui-progressbars", component: <UiProgressbar /> },
-  { path: "/ui-tabs-accordions", component: <UiTabsAccordions /> },
-  { path: "/ui-typography", component: <UiTypography /> },
-  { path: "/ui-video", component: <UiVideo /> },
-  { path: "/ui-session-timeout", component: <UiSessionTimeout /> },
-  { path: "/ui-rating", component: <UiRating /> },
-  { path: "/ui-rangeslider", component: <UiRangeSlider /> },
-  { path: "/ui-notifications", component: <UiNotifications /> },
-  { path: "/ui-offcanvas", component: <UiOffCanvas /> },
-  { path: "/ui-utilities", component: <UiUtilitie /> },
-  { path: "/ui-placeholders", component: <UiPlaceholders /> },
-  { path: "/ui-toasts", component: <UiToasts /> },
 
-  //Utility
-  { path: "/pages-starter", component: <PagesStarter /> },
-  { path: "/pages-timeline", component: <PagesTimeline /> },
-  { path: "/pages-faqs", component: <PagesFaqs /> },
-  { path: "/pages-pricing", component: <PagesPricing /> },
 
-  // this route should be at the end of all other routes
-  // eslint-disable-next-line react/display-name
+
   {
     path: "/",
     exact: true,
